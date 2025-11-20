42a73923ecc5acdf97554cae200a9696323ebec0
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-20 17:50:30 +0100

    Create Thousandcipheroneontwodmodelvalodatrdecode.h

---
+++ Thousandcipheroneontwodmodelvalodatrdecode.h
@@ -0,0 +1,3 @@
+//we can validate and force decoding range if the 
+//higher valies in another cipher is 10^15 or 10^17. using other valies is obvious
+//in lattices associated with models with no properties 
